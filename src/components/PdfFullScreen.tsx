b281315eb27abde2d23af51b276d5955fa65020e
toadmilk <48373163+toadmilk@users.noreply.github.com>
2024-08-10 16:30:58 +1200

    Completed PDF renderer.

---
+++ src/components/PdfFullScreen.tsx
@@ -0,0 +1,76 @@
+import { useState } from 'react';
+import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog';
+import { Button } from '@/components/ui/button';
+import { Expand, Loader2 } from 'lucide-react';
+import SimpleBar from 'simplebar-react';
+import { Document, Page } from 'react-pdf';
+import { useToast } from '@/components/ui/use-toast';
+import { useResizeDetector } from 'react-resize-detector';
+
+interface PdfFullScreenProps {
+  url: string;
+}
+
+const PdfFullScreen = ({ url }: PdfFullScreenProps) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [numPages, setNumPages] = useState<number>();
+
+  const { toast } = useToast();
+
+  const { width, ref } = useResizeDetector();
+
+  return (
+    <Dialog
+      isOpen={isOpen}
+      onDismiss={() => setIsOpen(false)}
+      onOpenChange={(v) => {
+        if (!v) {
+          setIsOpen(v);
+        }
+      }}
+      className="fixed inset-0 z-50"
+    >
+      <DialogTrigger
+        onClick={() => setIsOpen(true)}
+        asChild
+      >
+        <Button
+          aria-label="fullscreen"
+          variant="ghost"
+          className="gap-1.5"
+        >
+          <Expand className="w-4 h-4"/>
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="max-w-7xl w-full">
+        <SimpleBar autoHide={false} className="max-h-[calc(100vh-10rem)] mt-6">
+          <div ref={ref}>
+            <Document
+              loading={<Loader2 className="my-24 h-6 w-6 animate-spin"/>}
+              onLoadError={() => {
+                toast({
+                  title: 'Error loading PDF',
+                  description: 'Please try again later',
+                  variant: 'destructive',
+                });
+              }}
+              onLoadSuccess={({ numPages }) => setNumPages(numPages)}
+              file={url}
+              className="max-h-full"
+            >
+              {new Array(numPages).fill(0).map((_, i) => (
+                <Page
+                  key={i}
+                  width={width ? width : 1}
+                  pageNumber={i + 1}
+                />
+              ))}
+            </Document>
+          </div>
+        </SimpleBar>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default PdfFullScreen;
